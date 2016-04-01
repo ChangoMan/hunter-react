ee77a9eca32ff4d60e627c10b9ab9594c65dee80
Hunter Chang <hunter@madwiremedia.com>
2016-04-01 15:04:39 -0600

    map articles

--- src/pages/Articles.js
+++ src/pages/Articles.js
@@ -3,15 +3,37 @@ import Article from '../components/Article';
 
 export default class Articles extends Component {
     render() {
+
         console.log(this.props);
+
+        const Articles = [
+            "Article One",
+            "Article Two",
+            "Article Three",
+        ].map((title, i) => <Article key={i} title={title} />);
+
+        {/*
+        const Articles = [
+            <Article key={1} title="Article One" buttonText="Learn More" buttonText="Child Child Toggle BG" toggleMobile={this.props.toggleMobile} />,
+            <Article key={2} title="Article Two" buttonText="Learn More" />,
+            <Article key={3} title="Article Three" buttonText="Learn More" />
+        ];
+        */}
+
         return (
             <div>
                 <h2>Articles</h2>
                 <hr />
                 <div className="row">
+
+                    {Articles}
+
+                    {/*
                     <Article title="Article One" buttonText="Child Child Toggle BG" toggleMobile={this.props.toggleMobile} />
                     <Article title="Article Two" buttonText="Learn More" />
                     <Article title="Article Three" buttonText="Read More" />
+                    */}
+
                 </div>
             </div>
         );
