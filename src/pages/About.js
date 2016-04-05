8372ed91b902e1db7c29b0ae4e8f3b8aa4425ac0
Hunter Chang <hunter@madwiremedia.com>
2016-04-05 15:41:57 -0600

    small example on about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react';
 
 export default class About extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            liked: false,
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({liked: !this.state.liked});
+    }
+
     render() {
+
         console.log(this.props);
+        const text = this.state.liked ? 'like' : 'NO like';
+
         return (
             <div>
                 <h2>About</h2>
                 <p>{this.props.params.article}</p>
                 <p>{this.props.location.pathname}</p>
                 <button className="btn btn-warning" onClick={this.props.toggleMobile}>Children Toggle BG</button>
+                <hr />
+                <p onClick={this.handleClick}>You {text} this</p>
             </div>
         );
     }
